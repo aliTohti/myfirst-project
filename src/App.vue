95dbee282d880a812c1d1b625c570ae5ddabf5d1
aliTohti <1377044950@qq.com>
2022-03-19 21:41:57 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <maintab></maintab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import maintab from './components/maintabbar.vue'
+export default {
+  name: 'App',
+  components: {
+    maintab
+   
+  }
+}
+</script>
+
+<style>
+  @import './assets/css/all.css';
+ 
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+const home=()=>import('../views/home/home.vue')
+const clas=()=>import('../views/class/class.vue')
+const cart=()=>import('../views/cart/cart.vue')
+const user=()=>import('../views/user/user.vue')
+Vue.use(VueRouter)
+const routes=[
+  {
+      path:'',
+      redirect:'/home'
+  },
+  {
+      path:'/home',
+      component:home
+  },
+  {
+    path:'/class',
+    component:clas 
+  },
+  {
+    path:'/cart',
+    component:cart
+  },
+  {
+    path:'/user',
+    component:user
+  }
+]
+const router=new VueRouter({
+    routes,
+    mode: 'history',
+})
+export default router 
